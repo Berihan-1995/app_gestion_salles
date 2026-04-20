02f7bc087553ec92be0128c2d5b2d5dd13a8946d
berihqn mdcherif <3001555242@monboreal.ca>
2026-04-19 20:55:02 -0400

    service salle

---
+++ services/services_salle.py
@@ -0,0 +1,43 @@
+from data.dao_salle import DaoSalle
+from models.salle import Salle
+
+class ServiceSalle:
+    def __init__(self):
+        self.dao = DaoSalle()
+
+    def get_all_salles(self):
+        data = self.dao.charger_salles()
+        salles = []
+        for s in data:
+            salle = Salle(s["id_salle"], s["nom"], s["capacite"])
+            salles.append(salle)
+        return salles
+
+    def ajouter_salle(self, id_salle, nom, capacite):
+        salles = self.dao.charger_salles()
+
+        for s in salles:
+            if s["id_salle"] == id_salle:
+                raise ValueError("ID déjà utilisé")
+
+        if capacite <= 0:
+            raise ValueError("Capacité invalide")
+
+        nouvelle_salle = {
+            "id_salle": id_salle,
+            "nom": nom,
+            "capacite": capacite
+        }
+
+        salles.append(nouvelle_salle)
+        self.dao.sauvegarder_salles(salles)
+
+    def supprimer_salle(self, id_salle):
+        salles = self.dao.charger_salles()
+        nouvelles_salles = []
+
+        for s in salles:
+            if s["id_salle"] != id_salle:
+                nouvelles_salles.append(s)
+
+        self.dao.sauvegarder_salles(nouvelles_salles)
