cabb4f7bc772fbe1cdc3f324ca07c359a0aefc95
berihqn mdcherif <3001555242@monboreal.ca>
2026-04-19 23:12:33 -0400

    Implémentation du modèle Salle (code, description, categorie, capacite)

--- models/salle.py
+++ models/salle.py
@@ -1,14 +1,17 @@
-# cette classe Salle représente une salle avec son nom et sa capacité
+q
 
 class Salle:
-    def __init__(self, id_salle: int, nom: str, capacite: int):
-        self.id_salle = id_salle
-        self.nom = nom
+    def __init__(self, code: str, description: str, categorie: str, capacite: int):
+        self.code = code
+        self.description = description
+        self.categorie = categorie
         self.capacite = capacite
 
-    def __str__(self):
-        return f"Salle(id={self.id_salle}, nom='{self.nom}', capacite={self.capacite})"
+    def afficher_infos(self):
+        print(f"Code : {self.code}")
+        print(f"Description : {self.description}")
+        print(f"Catégorie : {self.categorie}")
+        print(f"Capacité : {self.capacite}")
 
     def __str__(self):
-        return f"Salle {self.id_salle} - {self.nom} ({self.capacite} places)"
-
+        return f"Salle({self.code}, {self.description}, {self.categorie}, {self.capacite})"
