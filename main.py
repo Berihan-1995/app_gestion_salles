5fcca92df6a10b3b518f466f3c35d3e259e5fccb
berihqn mdcherif <3001555242@monboreal.ca>
2026-04-19 22:32:37 -0400

    changement de menu principal avec l'option rechercher une salle

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ def main():
     vue = ViewSalle()
     choix = ""
 
-
-    while choix != "4":
+    while choix != "5":
         vue.afficher_menu()
         choix = input("Votre choix : ")
 
@@ -18,10 +17,12 @@ def main():
         elif choix == "3":
             vue.supprimer_salle()
         elif choix == "4":
+            vue.rechercher_salle()
+        elif choix == "5":
             print("Au revoir.")
         else:
             print("Choix invalide.")
 
-
 if __name__ == "__main__":
     main()
+
