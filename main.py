776c5c6973f47fcccfbf243b168a17ea360ecf96
berihqn mdcherif <3001555242@monboreal.ca>
2026-04-19 23:30:28 -0400

    lance l'interface graphique

--- main.py
+++ main.py
@@ -1,31 +1,4 @@
-# Ce programme affiche le menu de gestion des salles
-
 from views.view_salle import ViewSalle
 
-def main():
-    vue = ViewSalle()
-    choix = ""
-
-    while choix != "6":
-        vue.afficher_menu()
-        choix = input("Votre choix : ")
-
-        if choix == "1":
-            vue.afficher_salles()
-        elif choix == "2":
-            vue.ajouter_salle()
-        elif choix == "3":
-            vue.supprimer_salle()
-        elif choix == "4":
-            vue.modifier_salle()
-        elif choix == "5":
-            vue.rechercher_salle()
-        elif choix == "6":
-            print("Au revoir.")
-        else:
-            print("Choix invalide.")
-
-if __name__ == "__main__":
-    main()
-
-
+app = ViewSalle()
+app.mainloop()
